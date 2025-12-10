013e47abc65ed342b9e7c66598178e1e25cbb9d0
asawahli <126249023+asawahli@users.noreply.github.com>
2025-12-10 17:59:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import pd.io.formats.excel as pxl
+import pandas.io.formats.excel as pxl
 from io import BytesIO
 import pickle
 
@@ -223,5 +223,6 @@ if uploaded_file is not None:
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
             type="primary",
         )
+
 
 
